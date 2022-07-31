9b89067ba24d1fca78133408a08dc204ba46ce99
Abdullah108 <abdullahpro44@gmail.com>
2022-07-31 10:56:08 +0500

    space issue

--- index.js
+++ index.js
@@ -50,11 +50,13 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 // app.listen(PORT, () => console.log(`server running on port ${PORT}`));
-// if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './frontend/build')))
+if (process.env.NODE_ENV === 'production') {
+  
+  app.use(express.static(path.join(__dirname, 'frontend/build')))
+  
   app.get("*", (req,res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    console.log('working')
   })
-// }
+  
+}
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
